d15b121d5a4ddb2240f17a4ff239f562b80e0039
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2020-06-19 11:52:33 -0600

    Validate accepts, content type and body

--- routes/computers.js
+++ routes/computers.js
@@ -9,40 +9,148 @@ const { checkJwt, PERIPHERALS_PATH } = require("../config");
 /* ------------- Begin Controller Functions ------------- */
 router.get("/", checkJwt, async (req, res) => {
   const accepts = req.accepts("application/json");
-  // TODO: Validate accepts
-  computers.get_by_property(req, "user", req.user.sub).then(async (data) => {
-    data.items.map((entity) => {
-      entity.self = `${req.protocol}://${req.get("host")}${req.baseUrl}/${
-        entity.id
-      }`;
+
+  if (!accepts) {
+    res.status(406).send("Not Acceptable");
+  } else {
+    computers.get_by_property(req, "user", req.user.sub).then(async (data) => {
+      data.items.map((entity) => {
+        entity.self = `${req.protocol}://${req.get("host")}${req.baseUrl}/${
+          entity.id
+        }`;
+      });
+
+      for (let entity of data.items) {
+        const children = await peripherals.get_by_property(
+          "computer",
+          entity.id
+        );
+        children && children.length
+          ? (entity.peripherals = children.map(({ id }) => ({
+              id,
+              self: `${req.protocol}://${req.get(
+                "host"
+              )}${PERIPHERALS_PATH}/${id}`,
+            })))
+          : (entity.peripherals = []);
+      }
+
+      if (data.next) {
+        data.next = `${req.protocol}://${req.get("host")}${
+          req.baseUrl
+        }?cursor=${data.next}`;
+      }
+
+      res.status(200).json(data);
     });
-
-    for (let entity of data.items) {
-      const children = await peripherals.get_by_property("computer", entity.id);
-      children && children.length
-        ? (entity.peripherals = children.map(({ id }) => ({
-            id,
-            self: `${req.protocol}://${req.get(
-              "host"
-            )}${PERIPHERALS_PATH}/${id}`,
-          })))
-        : (entity.peripherals = []);
-    }
-
-    if (data.next) {
-      data.next = `${req.protocol}://${req.get("host")}${req.baseUrl}?cursor=${
-        data.next
-      }`;
-    }
-
-    res.status(200).json(data);
-  });
+  }
 });
 
 router.get("/:id", checkJwt, (req, res) => {
-  computers
-    .get_by_property(req, "__key__", req.params.id)
-    .then(async (data) => {
+  const accepts = req.accepts("application/json");
+
+  if (!accepts) {
+    res.status(406).send("Not Acceptable");
+  } else {
+    computers
+      .get_by_property(req, "__key__", req.params.id)
+      .then(async (data) => {
+        if (!data.items || data.items.length !== 1) {
+          // Set the status code to 403 if a protected resource was not found.
+          res.status(403).end();
+        } else if (data.items[0].user && data.items[0].user !== req.user.sub) {
+          // Set the status code to 403 if a protected resource does not belong to the current user.
+          res.status(403).end();
+        } else {
+          const entity = data.items[0];
+          entity.self = `${req.protocol}://${req.get("host")}${req.baseUrl}/${
+            entity.id
+          }`;
+
+          const children = await peripherals.get_by_property(
+            "computer",
+            entity.id
+          );
+          children && children.length
+            ? (entity.peripherals = children.map(({ id }) => ({
+                id,
+                self: `${req.protocol}://${req.get(
+                  "host"
+                )}${PERIPHERALS_PATH}/${id}`,
+              })))
+            : (entity.peripherals = []);
+
+          res.status(200).json(entity);
+        }
+      });
+  }
+});
+
+router.post("/", checkJwt, (req, res) => {
+  const accepts = req.accepts("application/json");
+
+  if (req.get("Content-Type") !== "application/json") {
+    res.status(415).send("Server only accepts application/json data");
+  } else if (!accepts) {
+    res.status(406).send("Not Acceptable");
+  } else if (req.body.id || req.body.self) {
+    res.status(400).json({
+      Error:
+        "The request object attempted to specify one or more idempotent attributes",
+    });
+  } else if (
+    !req.body.manufacturer ||
+    !req.body.model ||
+    !req.body.serial_number
+  ) {
+    res.status(400).json({
+      Error:
+        "The request object is missing at least one of the required attributes",
+    });
+  } else {
+    const entity = {
+      manufacturer: req.body.manufacturer,
+      model: req.body.model,
+      serial_number: req.body.serial_number,
+      user: req.user.sub,
+    };
+
+    computers.post_one(entity).then((entity) => {
+      entity.self = `${req.protocol}://${req.get("host")}${req.baseUrl}/${
+        entity.id
+      }`;
+
+      // Append an empty array rather than querying for related non-user entities,
+      // since this relationship can't exist yet.
+      entity.peripherals = [];
+
+      res.status(201).json(entity);
+    });
+  }
+});
+
+router.patch("/:id", checkJwt, (req, res) => {
+  const accepts = req.accepts("application/json");
+
+  if (req.get("Content-Type") !== "application/json") {
+    res.status(415).send("Server only accepts application/json data");
+  } else if (!accepts) {
+    res.status(406).send("Not Acceptable");
+  } else if (req.body.id || req.body.self) {
+    res.status(400).json({
+      Error:
+        "The request object attempted to specify one or more idempotent attributes",
+    });
+  } else if (
+    !req.body.manufacturer &&
+    !req.body.model &&
+    !req.body.serial_number
+  ) {
+    res.status(400).json({
+      Error: "The request object contains no modifiable attribute",
+    });
+  } else {
+    computers.get_by_property(req, "__key__", req.params.id).then((data) => {
       if (!data.items || data.items.length !== 1) {
         // Set the status code to 403 if a protected resource was not found.
         res.status(403).end();
@@ -50,126 +158,98 @@ router.get("/:id", checkJwt, (req, res) => {
         // Set the status code to 403 if a protected resource does not belong to the current user.
         res.status(403).end();
       } else {
-        const entity = data.items[0];
-        entity.self = `${req.protocol}://${req.get("host")}${req.baseUrl}/${
-          entity.id
-        }`;
+        const originalEntity = data.items[0];
+        const updatedEntity = {
+          id: req.params.id,
+          manufacturer: req.body.manufacturer || originalEntity.manufacturer,
+          model: req.body.model || originalEntity.model,
+          serial_number: req.body.serial_number || originalEntity.serial_number,
+          user: req.user.sub,
+        };
+        computers.update_one(updatedEntity).then(async (entity) => {
+          entity.self = `${req.protocol}://${req.get("host")}${req.baseUrl}/${
+            entity.id
+          }`;
 
-        const children = await peripherals.get_by_property(
-          "computer",
-          entity.id
-        );
-        children && children.length
-          ? (entity.peripherals = children.map(({ id }) => ({
-              id,
-              self: `${req.protocol}://${req.get(
-                "host"
-              )}${PERIPHERALS_PATH}/${id}`,
-            })))
-          : (entity.peripherals = []);
-
-        res.status(200).json(entity);
+          const children = await peripherals.get_by_property(
+            "computer",
+            entity.id
+          );
+          children && children.length
+            ? (entity.peripherals = children.map(({ id }) => ({
+                id,
+                self: `${req.protocol}://${req.get(
+                  "host"
+                )}${PERIPHERALS_PATH}/${id}`,
+              })))
+            : (entity.peripherals = []);
+          res.status(200).json(entity);
+        });
       }
     });
-});
-
-router.post("/", checkJwt, (req, res) => {
-  const entity = {
-    manufacturer: req.body.manufacturer,
-    model: req.body.model,
-    serial_number: req.body.serial_number,
-    user: req.user.sub,
-  };
-
-  computers.post_one(entity).then((entity) => {
-    entity.self = `${req.protocol}://${req.get("host")}${req.baseUrl}/${
-      entity.id
-    }`;
-    // Append an empty array rather than querying for related non-user entities,
-    // since this relationship can't exist yet.
-    entity.peripherals = [];
-    res.status(201).json(entity);
-  });
-});
-
-router.patch("/:id", checkJwt, (req, res) => {
-  computers.get_by_property(req, "__key__", req.params.id).then((data) => {
-    if (!data.items || data.items.length !== 1) {
-      // Set the status code to 403 if a protected resource was not found.
-      res.status(403).end();
-    } else if (data.items[0].user && data.items[0].user !== req.user.sub) {
-      // Set the status code to 403 if a protected resource does not belong to the current user.
-      res.status(403).end();
-    } else {
-      const originalEntity = data.items[0];
-      const updatedEntity = {
-        id: req.params.id,
-        manufacturer: req.body.manufacturer || originalEntity.manufacturer,
-        model: req.body.model || originalEntity.model,
-        serial_number: req.body.serial_number || originalEntity.serial_number,
-        user: req.user.sub,
-      };
-      computers.update_one(updatedEntity).then(async (entity) => {
-        entity.self = `${req.protocol}://${req.get("host")}${req.baseUrl}/${
-          entity.id
-        }`;
-
-        const children = await peripherals.get_by_property(
-          "computer",
-          entity.id
-        );
-        children && children.length
-          ? (entity.peripherals = children.map(({ id }) => ({
-              id,
-              self: `${req.protocol}://${req.get(
-                "host"
-              )}${PERIPHERALS_PATH}/${id}`,
-            })))
-          : (entity.peripherals = []);
-        res.status(200).json(entity);
-      });
-    }
-  });
+  }
 });
 
 router.put("/:id", checkJwt, (req, res) => {
-  computers.get_by_property(req, "__key__", req.params.id).then((data) => {
-    if (!data.items || data.items.length !== 1) {
-      // Set the status code to 403 if a protected resource was not found.
-      res.status(403).end();
-    } else if (data.items[0].user && data.items[0].user !== req.user.sub) {
-      // Set the status code to 403 if a protected resource does not belong to the current user.
-      res.status(403).end();
-    } else {
-      const entity = {
-        id: req.params.id,
-        manufacturer: req.body.manufacturer,
-        model: req.body.model,
-        serial_number: req.body.serial_number,
-        user: req.user.sub,
-      };
-      computers.update_one(entity).then(async (entity) => {
-        entity.self = `${req.protocol}://${req.get("host")}${req.baseUrl}/${
-          entity.id
-        }`;
+  const accepts = req.accepts("application/json");
 
-        const children = await peripherals.get_by_property(
-          "computer",
-          entity.id
-        );
-        children && children.length
-          ? (entity.peripherals = children.map(({ id }) => ({
-              id,
-              self: `${req.protocol}://${req.get(
-                "host"
-              )}${PERIPHERALS_PATH}/${id}`,
-            })))
-          : (entity.peripherals = []);
+  if (req.get("Content-Type") !== "application/json") {
+    res.status(415).send("Server only accepts application/json data");
+  } else if (!accepts) {
+    res.status(406).send("Not Acceptable");
+  } else if (
+    !req.body.manufacturer ||
+    !req.body.model ||
+    !req.body.serial_number
+  ) {
+    res.status(400).json({
+      Error:
+        "The request object is missing at least one of the required attributes",
+    });
+  } else if (req.body.id || req.body.self) {
+    res.status(400).json({
+      Error:
+        "The request object attempted to specify one or more idempotent attributes",
+    });
+  } else {
+    computers.get_by_property(req, "__key__", req.params.id).then((data) => {
+      if (!data.items || data.items.length !== 1) {
+        // Set the status code to 403 if a protected resource was not found.
+        res.status(403).end();
+      } else if (data.items[0].user && data.items[0].user !== req.user.sub) {
+        // Set the status code to 403 if a protected resource does not belong to the current user.
+        res.status(403).end();
+      } else {
+        const entity = {
+          id: req.params.id,
+          manufacturer: req.body.manufacturer,
+          model: req.body.model,
+          serial_number: req.body.serial_number,
+          user: req.user.sub,
+        };
+        computers.update_one(entity).then(async (entity) => {
+          entity.self = `${req.protocol}://${req.get("host")}${req.baseUrl}/${
+            entity.id
+          }`;
 
-        res.status(200).json(entity);
-      });
-    }
-  });
+          const children = await peripherals.get_by_property(
+            "computer",
+            entity.id
+          );
+          children && children.length
+            ? (entity.peripherals = children.map(({ id }) => ({
+                id,
+                self: `${req.protocol}://${req.get(
+                  "host"
+                )}${PERIPHERALS_PATH}/${id}`,
+              })))
+            : (entity.peripherals = []);
+
+          res.status(200).json(entity);
+        });
+      }
+    });
+  }
 });
 
 router.put("/:computer_id/peripherals/:peripheral_id", (req, res) => {
