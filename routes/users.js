17fefc5caca5253d7bbdf6c47ede910e4910640c
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2020-06-18 21:13:43 -0600

    Remove unused code

--- routes/users.js
+++ routes/users.js
@@ -10,22 +10,10 @@ router.get("/", (req, res) => {
   users.get_all().then((data) => res.status(200).json(data));
 });
 
-router.get("/:user_id/computers", checkJwt, (req, res) => {
-  let statusCode = 200;
-  computers
-    .get_by_property("user", req.user.sub)
-    .then((data) => res.status(statusCode).json(data));
-});
-
 router.all("/", (req, res) => {
   res.set("Allow", "GET");
   res.status(405).end();
 });
-
-// router.all("/:user_id", (req, res) => {
-//   res.set("Allow", "GET, PATCH, PUT, DELETE");
-//   res.status(405).end();
-// });
 
 /* ------------- End Controller Functions ------------- */
 
